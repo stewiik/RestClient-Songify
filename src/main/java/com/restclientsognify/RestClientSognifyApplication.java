0eae649f3a6b82b67924ece21de1c7ff0c684121
Wiktoria <w.stec19@gmail.com>
2024-07-18 10:48:41 +0200

    first commit

---
+++ src/main/java/com/restclientsognify/RestClientSognifyApplication.java
@@ -0,0 +1,13 @@
+package com.restclientsognify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientSognifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientSognifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restclientsognify/RestClientSognifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restclientsognify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientSognifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
